a730f42ca28186669ec8eb66341f7766431e2817
shiv4920 <jaybodhkumar4920@gmail.com>
2026-09-10 00:59:23 +0530

    Time: 0 ms (100%), Space: 9 MB (57.33%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -3,15 +3,9 @@ public:
     long long countCommas(long long n) {
         long long lower=1000;
         long long  count=0;
-        long long comm=1;
         while(lower<=n){
-            long long upper=lower*1000-1;
-            if(upper>n){
-             upper=n;
-            }
-            count+=(upper-lower+1)*comm;
+            count+=n-lower+1;
             lower=lower*1000;
-            comm++;
         }
         return count;
     }
